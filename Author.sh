0eefca04d843dda96a7bb710e65635217d027c3b
CATHY555 <catherine.kg@cabotsolutions.com>
2019-09-20 14:04:25 +0530

    Author updated

--- Author.sh
+++ Author.sh
@@ -2,14 +2,15 @@
 
 git filter-branch --env-filter '
 
-OLD_NAME=“CATHERINE”
-CORRECT_NAME=“CATHY”
+OLD_NAME="CATHERINE"
+CORRECT_NAME="CATHY"
+EMAIL="catherine.kg@cabotsolutions.com"
 
-if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_NAME" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
 fi
-if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_NAME" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
 fi
